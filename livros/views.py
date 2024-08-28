321fc28eab109b3b908bdbe6b0e928fb88e0bc8f
PROGRAMAÇÃO EM PYTHON 2024.3 <51129532024.3@senacrjedu.local>
2024-08-28 20:40:50 -0300

    version 4

--- livros/views.py
+++ livros/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import  reverse_lazy
 # Create your views here.
 from .models import Livros
@@ -9,4 +9,20 @@ class LivrosListView(ListView):
 class LivrosCreateView(CreateView):
     model = Livros
     fields = ['nome','imagem','editora','autor','publicado_em']
+    success_url = reverse_lazy('livros_list')
+
+
+class LivrosLancamento(ListView):
+    model= Livros
+    template_name = 'livros/livros_lan.html'
+
+class LivrosUpdateView(UpdateView):
+    model = Livros
+    fields = ['nome', 'imagem', 'editora', 'autor', 'publicado_em']
+    template_name = 'livros/livros_form.html'
+    success_url = reverse_lazy('livros_list')
+
+class LivrosDeleteView(DeleteView):
+    model = Livros
+    template_name = 'livros/livros_delete.html'
     success_url = reverse_lazy('livros_list')
